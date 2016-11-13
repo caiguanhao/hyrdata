4336217436c4662ffdb0be2ebd00d26fe487b11b
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2016-11-13 17:19:06 +0800

    eager load lib

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Hyrdata
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W[#{config.root}/lib]
+    config.eager_load_paths += %W[#{config.root}/lib]
   end
 end
