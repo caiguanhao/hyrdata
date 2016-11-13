85d3999a57f0557d380a685831f0a9b53d84e006
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2016-11-13 16:04:55 +0800

    update environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_cable.allowed_request_origins = [/http?:\/\/localhost:\d+/]
+  config.action_cable.allowed_request_origins = [/https?:\/\/localhost:\d+/]
 end
